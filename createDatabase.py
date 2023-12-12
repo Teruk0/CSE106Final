bf7686cd8d290e6d20556faf246d4b25c052e6e3
jgalindo24 <113061159+jgalindo24@users.noreply.github.com>
2023-12-11 18:35:36 -0800

    Adding database python creation file & files needed for use of mySQL/Node.js

---
+++ createDatabase.py
@@ -0,0 +1,77 @@
+import sqlite3
+from sqlite3 import Error
+
+def createTable(_conn):
+    _cur = _conn.cursor()
+    _cur.execute("""
+    create table if not exists credential (
+        c_id integer primary key,
+        c_name char(100),
+        c_username char(100) not null,
+        c_password char(100) not null
+    );
+    """)
+
+    _cur.execute("""
+    create table if not exists post (
+        p_id integer primary key,
+        p_userId char(100) not null,
+        p_question char(300) not null,
+        p_upvotes char(100) not null,
+        p_downvotes char(100) not null
+    );
+    """)
+
+    _cur.execute("""
+    create table if not exists response (
+        r_id integer primary key,
+        r_postId char(100) not null,
+        p_response char(300) not null,
+        p_upvotes integer not null,
+        p_downvotes integer not null
+    );
+    """)
+    _conn.commit()
+
+    _cur.close()
+
+
+def dropTable(_conn):
+    _cur = _conn.cursor()
+
+    _cur.execute("""
+    drop table if exists credentials
+    """)
+
+    _cur.execute("""
+    drop table if exists post
+    """)
+
+    _cur.execute("""
+    drop table if exists response
+    """)
+
+    _conn.commit()
+    _cur.close()
+
+
+
+def main():
+    _conn = None
+    try:
+        _conn = sqlite3.connect("forumData.sqlite")
+        print("successful connection to Database")
+    except Error as e:
+        print(e)
+    
+    dropTable(_conn)
+    createTable(_conn)
+
+    try:
+        _conn.close()
+        print("successful closure of Database")
+    except Error as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Assuming that 'courseInfo.sqlite' is in the same directory as 'server.js'
+const dbPath = path.resolve(__dirname, 'forumData.sqlite');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Error opening database:', err.message);
+  } else {
+    console.log('Database opened successfully');
+  }
+});
+
+// Enable foreign key support
+db.run('PRAGMA foreign_keys = ON');
+
+// Parse POST request data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Default HTML file: Login.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+  });
+// CSS file
+app.use(express.static(path.join(__dirname)));
+
+// Handle form submission
+// Inside your login route
+// app.post('/login', (req, res) => {
+//     const { username, password } = req.body;
+  
+//     // Replace this logic with your actual authentication logic (querying the database, etc.)
+//     const query = `SELECT c_username, c_password FROM credentials WHERE c_username = ? AND c_password = ?`;
+//     db.get(query, [username, password], (err, row) => {
+//       if (err) {
+//         console.error('Error querying database:', err.message);
+//         res.red('/?error=1');
+//         return;
+//       }
+  
+//       if (row) {
+//         const { username, password } = row;
+//         if (role === 'instructor') {
+//           res.redirect(`/forum/${username}`);  // Use the username here
+//         }
+//       } else {
+//         res.redirect('/?error=1');
+//       }
+//     });
+//   });
+
+  app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    console.log(username)
+    console.log(password)
+    // Replace this logic with your actual authentication logic (querying the database, etc.)
+    db.run(`INSERT INTO credential (c_username, c_password) VALUES (?, ?)`, [username, password], (err, row) => {
+      if (err) {
+        console.log('Error querying database:', err.message);
+        res.status(500).json({ error: 'Internal Server Error', details: err.message });        return;
+      } else {
+        console.log('Data added Successfully');
+        res.status(200).json({ message: 'Data added Successfully' });
+      }
+    });
+  });
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+    });
